f3f8be47204221792b4b72088893a012a47a4d78
Johnny <johnjritenour@gmail.com>
2021-05-06 14:46:41 -0500

    Update permissions.ps1

--- permissions.ps1
+++ permissions.ps1
@@ -1,7 +1,7 @@
  #Login to file server and identify shared drive to be reviewed.
  
  #Get existing file share permissions via this line
- get-acol -path d:\folder | format-list
+ get-acl -path d:\folder | format-list
  
  #Use below script to remove access to shared folder/file
  #Ensure you edit the path under $acl and set-acl -Path as well as the domain\username
